c2ddba9187a8d3f361cbcd25496e378becac9019
Danil Antoshkin <TwerTrue@yandex.ru>
2023-06-08 17:54:32 +0300

    Финальные правки

--- app.js
+++ app.js
@@ -13,33 +13,41 @@ function debounce(f, ms) {
     };
 }
 
-const getRepositories = function(value) {
-    fetch(`https://api.github.com/search/repositories?q=${value}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Ошибка запроса!');
-            }
-            return response.json();
-        })
-        .then((data) => {
-            let repositories = data.items.filter((dataItem, dataItemIndex) => {
-                if (dataItemIndex < 5) {
-                    return true;
-                }
-            })
-            addRepositories(repositories);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+// Получаем репозитории по запросу
+async function getRepositories (value) {
+    let response = await fetch(`https://api.github.com/search/repositories?q=${value}`)
+    .then((response) => {
+        // Проверяем успешность запроса и выкидываем ошибку
+        if (!response.ok) {
+            throw new Error('Ошибка запроса!')
+        }
+
+        return response.json();
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+    // Нам нужно только 5 репозиториев
+    let repositories = [];
+
+    for (let i = 0; i < 5; i++) {
+        repositories.push(response.items[i]);
+    }
+
+    return repositories;
 };
 
+// Добавляем репозиторий в избранное
 const clickAddStarred = function (event) {
     let target = event.target;
-    console.log(target);
-    addStarred();
+    console.dir(target);
+    addStarred(target);
+    searchInput.value = ``;
+    removeRepositories();
 }
 
+// Очищаем автокомплит
 const removeRepositories = function() {
     search.classList.remove(`active`);
     if (searchAutoComplete.firstChild) {
@@ -49,18 +57,21 @@ const removeRepositories = function() {
     searchAutoComplete.removeEventListener(`click`, clickAddStarred);
 }
 
-searchInput.addEventListener("input", debounce(function(event) {
-    removeRepositories();
-    let request = searchInput.value;
+searchInput.addEventListener("keyup", debounce(async function(event) {
+    let repositories; // Репозитории по запросу
+    let request = searchInput.value; // Значение в поле ввода
+
     if (event.keyCode !== 32 && request.length !== 0) {
-        getRepositories(request);
+        repositories = await getRepositories(request);
+        addRepositories(repositories);
+        console.log(repositories);
     }
-}, 1500));
+}, 400));
 
 
 
 const addRepositories = function(repositories) {
-    search.classList.add(`active`)
+    removeRepositories();
     if (repositories.length === 0) {
         const item = document.createElement(`li`);
         item.textContent = `Ничего не найдено =(`;
@@ -69,31 +80,37 @@ const addRepositories = function(repositories) {
         const repositoriesList = document.createDocumentFragment();
         const itemUL = document.createElement(`ul`);
         repositoriesList.appendChild(itemUL);
-        repositories.forEach((repositoriesItem, repositoriesItemIndex) => {
+        repositories.forEach((repositoriesItem) => {
             const itemLI = document.createElement(`li`);
             itemLI.textContent = repositoriesItem.name;
+            itemLI.gitHubName = repositoriesItem.name;
+            itemLI.gitHubOwner = repositoriesItem.owner.login;
+            itemLI.gitHubStarred = repositoriesItem.stargazers_count;            ;
             itemUL.appendChild(itemLI);
         })
 
         searchAutoComplete.appendChild(repositoriesList);
         searchAutoComplete.addEventListener(`click`, clickAddStarred);
+
     }
+
+    search.classList.add(`active`); // Показываем репозитории
 }
 
-const addStarred = function () {
+const addStarred = function (target) {
     const starredCard = document.createDocumentFragment();
 
     const cardDiv = document.createElement(`div`);
     cardDiv.classList.add(`starred-card`)
     starredCard.appendChild(cardDiv);
     const cardName = document.createElement(`p`);
-    cardName.textContent = `Test`;
+    cardName.textContent = `Имя репозитория: ${target.gitHubName}`;
     cardDiv.appendChild(cardName);
     const cardOwner = document.createElement(`p`);
-    cardOwner.textContent = `Test`;
+    cardOwner.textContent = `Владелец: ${target.gitHubOwner}`;
     cardDiv.appendChild(cardOwner);
     const cardStars = document.createElement(`p`);
-    cardStars.textContent = `Test`;
+    cardStars.textContent = `Количество звёзд: ${target.gitHubStarred}`;
     cardDiv.appendChild(cardStars);
     const cardButton = document.createElement(`button`);
     cardButton.classList.add(`card__button`)
